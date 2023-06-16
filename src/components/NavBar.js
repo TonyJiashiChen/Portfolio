b25ffca34447db77e3bf952566cac9d8e0496ced
TonyJiashiChen <tonychen9661@gmail.com>
2023-06-16 15:08:18 +1000

    changes

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,7 +1,6 @@
 import { useState, useEffect } from "react";
 import { Navbar, Container, Nav } from "react-bootstrap";
 
-//temp
 
 export const NavBar = () => {
   const [activeLink, setActiveLink] = useState("home");
