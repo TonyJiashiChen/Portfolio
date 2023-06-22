22ced077ec070b124b9749b7cb0d766169d4b164
TonyJiashiChen <tonychen9661@gmail.com>
2023-06-22 18:04:46 +1000

    banner

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -4,8 +4,47 @@ import { ArrowRightCircle } from "react-bootstrap-icons";
 import headerImg from "../assets/img/header-img.svg";
 
 export const Banner = () => {
+  // this part I don't really understand
+  const [loopNum, setLoopNum] = useState(0);
+  const [isDeleting, setIsDeleting] = useState(false);
   const toRotate = ["Web Developer", "Software Developer", "Cloud Engineer"];
-  
+  const [text, setText] = useState("");
+  const [delta, setDelta] = useState(300 - Math.random() * 100);
+  const period = 2000;
+
+  useEffect(() => {
+    let ticker = setInterval(() => {
+      tick();
+    }, delta);
+
+    return () => {
+      clearInterval(ticker);
+    };
+  }, [text]);
+
+  const tick = () => {
+    let i = loopNum % toRotate.length;
+    let fullText = toRotate[i];
+    let updatedText = isDeleting
+      ? fullText.substring(0, text.length - 1)
+      : fullText.substring(0, text.length + 1);
+
+    setText(updatedText);
+
+    if (isDeleting) {
+      setDelta((prevDelta) => prevDelta / 2);
+    }
+
+    if (!isDeleting && updatedText === fullText) {
+      setIsDeleting(true);
+      setDelta(period);
+    } else if (isDeleting && updatedText === "") {
+      setIsDeleting(false);
+      setLoopNum(loopNum + 1);
+      setDelta(500);
+    }
+  };
+
   return (
     <section className="banner" id="home">
       <Container>
@@ -14,13 +53,12 @@ export const Banner = () => {
             <span className="tagline">Welcome to my Portfolio</span>
             <h1>
               {`Hi I'm Tony`}
-              <span className="wrap">Software Developer</span>
+              <span className="wrap">{text}</span>
             </h1>
             <p>
               Final year Master of Information Technology student with
               experience working as software engineer in the blockchain
-              industry. Awarded research scholarship 
-              and completed IT industry
+              industry. Awarded research scholarship and completed IT industry
               placement with Monash University where I gained both programming
               and leadership skills. Looking for a role in the IT industry as a
               full-stack or back-end developer which will allow me to keep
