60e7db5a02f8b27302ace08f969e6e6d2e712aa0
TonyJiashiChen <tonychen9661@gmail.com>
2023-12-31 22:35:13 +1100

    update description

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -69,14 +69,13 @@ export const Banner = () => {
                     <span className="wrap">{text}</span>
                   </h1>
                   <p>
-                    Final year Master of Information Technology student with
-                    experience working as software engineer in the blockchain
-                    industry. Awarded research scholarship and completed IT
-                    industry placement with Monash University where I gained
-                    both programming and leadership skills. Looking for a role
-                    in the IT industry as a full-stack or back-end developer
-                    which will allow me to keep developing and exploring new
-                    technologies.
+                    Master of Information Technology graduate with experience
+                    working as software engineer in the blockchain industry.
+                    Awarded research scholarship and completed IT industry
+                    placement with Monash University where I gained both
+                    programming and leadership skills. Looking for a role in the
+                    IT industry as a full-stack or back-end developer which will
+                    allow me to keep developing and exploring new technologies.
                   </p>
                   <button onClick={redirectToLinkedIn}>
                     Let's connect <ArrowRightCircle size={25} />
